c15f6f0eb563ab17ab2c854ca24706d74a58e7b5
Vicky Kumar <imvickykumar999@gmail.com>
2025-12-25 11:05:30 +0000

    Fix: update views imports

--- monitoring/views.py
+++ monitoring/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
-from django_filters import FilterSet, ChoiceFilter
+from django_filters import FilterSet, ChoiceFilter, CharFilter
 from rest_framework.filters import OrderingFilter
 from django.contrib.auth import get_user_model
 
